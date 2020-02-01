37c3cb0464f3fabcc0e768c2f49eb981d9c4930d
rhon3n <baronontherhine@gmail.com>
2020-02-01 11:25:19 -0800

    added react-bootstrap

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class App extends React.Component {
   render () {
     return (
-      <div><h1>Hello, React in Electron!</h1></div>
+      <Jumbotron>
+        <Container>
+          <Row className="justify-content-center">
+            <h1>React in Electron!</h1>
+          </Row>
+        </Container>
+      </Jumbotron>
     )
   }
 }
